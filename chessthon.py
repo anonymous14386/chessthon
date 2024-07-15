a66f211f9090601a724f7caccddbb099d6b8f020
Dionysus <ndiramio@gmail.com>
2024-07-15 02:25:49 -0400

    Create chessthon.py

---
+++ chessthon.py
@@ -0,0 +1,123 @@
+#board
+print("8 ■ □ ■ □ ■ □ ■ □")
+print("7 □ ■ □ ■ □ ■ □ ■")
+print("6 ■ □ ■ □ ■ □ ■ □")
+print("5 □ ■ □ ■ □ ■ □ ■")
+print("4 ■ □ ■ □ ■ □ ■ □")
+print("3 □ ■ □ ■ □ ■ □ ■")
+print("2 ■ □ ■ □ ■ □ ■ □")
+print("1 □ ■ □ ■ □ ■ □ ■")
+print("  a b c d e f g h")
+ 
+wKing = "♔"
+wQueen = "♕"
+wRook1 = "♖"
+wRook2 = "♖"
+wBishop1 = "♗"
+wBishop2 = "♗"
+wKnight1 = "♘"
+wKnight2 = "♘"
+wPawn1 = "♙"
+wPawn2 = "♙"
+wPawn3 = "♙"
+wPawn4 = "♙"
+wPawn5 = "♙"
+wPawn6 = "♙"
+wPawn7 = "♙"
+wPawn8 = "♙"
+wSquare = "■"
+ 
+bKing = "♚"
+bQueen = "♛"
+bRook1 = "♜"
+bRook2 = "♜"
+bBishop1 = "♝"
+bBishop2 = "♝"
+bKnight1 = "♞"
+bKnight2 = "♞"
+bPawn1 = "♟︎"
+bPawn2 = "♟︎"
+bPawn3 = "♟︎"
+bPawn4 = "♟︎"
+bPawn5 = "♟︎"
+bPawn6 = "♟︎"
+bPawn7 = "♟︎"
+bPawn8 = "♟︎"
+bSquare = "□"
+ 
+ 
+#ONE CLAP THIS TIME
+#Board setup default idk I just work here
+#define the board because you a hoe
+ 
+a1 = 
+b1 =
+c1 =
+d1 =
+e1 =
+f1 =
+g1 =
+h1 =
+ 
+a2 = 
+b2 =
+c1 =
+d1 =
+e1 =
+f1 =
+g1 =
+h1 =
+ 
+a1 = 
+b1 =
+c1 =
+d1 =
+e1 =
+f1 =
+g1 =
+h1 =
+ 
+a1 = 
+b1 =
+c1 =
+d1 =
+e1 =
+f1 =
+g1 =
+h1 =
+ 
+a1 = 
+b1 =
+c1 =
+d1 =
+e1 =
+f1 =
+g1 =
+h1 =
+ 
+a1 = 
+b1 =
+c1 =
+d1 =
+e1 =
+f1 =
+g1 =
+h1 =
+ 
+a1 = 
+b1 =
+c1 =
+d1 =
+e1 =
+f1 =
+g1 =
+h1 =
+ 
+a1 = 
+b1 =
+c1 =
+d1 =
+e1 =
+f1 =
+g1 =
+h1 =
